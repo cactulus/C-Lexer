e0f65eaed0ea9965977299fb1aa635152dfd51d6
cactulus <Nikolas@Kaipel.net>
2023-01-19 21:36:15 +0100

    added remaining tokens

--- lex.c
+++ lex.c
@@ -95,20 +95,37 @@ enum token_type {
 
 	/* three char operators*/
 	T_LSH_EQ,
-	T_RSH_EQ
+	T_RSH_EQ,
+	T_DOT_DOT_DOT
 };
 
 enum states {
 	S_INITIAL = 0,
-	S_SPACE = 1,
-	S_ID = 2,
-	S_INT = 3,
-	S_FLOAT = 4,
-	S_SC_OP = 5,
-	S_CHAR = 6,
-	S_STRING = 7,
-	S_EQ = 8,
-	S_EQUABLE = 9,
+	S_SPACE,
+	S_ID,
+	S_INT,
+	S_FLOAT,
+	S_SC_OP,
+	S_CHAR,
+	S_STRING,
+	S_EQ,
+	S_EQUABLE,
+	S_PLUS,
+	S_MINUS,
+	S_STAR,
+	S_SLASH,
+	S_AND,
+	S_OR,
+	S_GREATER,
+	S_LESSER,
+	S_SL_COMMENT,
+	S_SL_COMMENT_END,
+	S_ML_COMMENT,
+	S_ML_COMMENT_END,
+	S_DOT,
+	S_DOT_DOT,
+	S_LSH,
+	S_RSH,
 
 	S_FINAL_ID,
 	S_END_OF_FILE,
@@ -118,7 +135,11 @@ enum states {
 	S_FINAL_STRING,
 	S_FINAL_CHAR,
 	S_FINAL_SC,
-	S_FINAL_TC
+	S_FINAL_TC,
+
+	S_DOT_DOT_DOT,
+	S_LSH_EQUALS,
+	S_RSH_EQUALS,
 };
 
 enum equivalence_classes {
@@ -137,11 +158,10 @@ enum equivalence_classes {
 	EC_SLASH = 12,
 	EC_AND = 13,
 	EC_OR = 14,
-	EC_HASH = 15,
-	EC_GREATER = 16,
-	EC_LESSER = 17,
-	EC_QUOTE = 18,
-	EC_DOUBLE_QUOTES = 19,
+	EC_GREATER = 15,
+	EC_LESSER = 16,
+	EC_QUOTE = 17,
+	EC_DOUBLE_QUOTES = 18,
 };
 
 const u8 equivalence_class[] = {
@@ -149,63 +169,116 @@ const u8 equivalence_class[] = {
 	0, EC_SPACE, EC_LF, 0, 0, EC_SPACE, 0, 0,
 	0, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 0, 0, 0, 0, 0, 0,
-	EC_SPACE, EC_EQUABLE, EC_DOUBLE_QUOTES, EC_HASH, 0, EC_EQUABLE, EC_AND, EC_QUOTE,
+	EC_SPACE, EC_EQUABLE, EC_DOUBLE_QUOTES, EC_SC_OP, 0, EC_EQUABLE, EC_AND, EC_QUOTE,
 	EC_SC_OP, EC_SC_OP, EC_STAR, EC_PLUS, EC_SC_OP, EC_MINUS, EC_DOT, EC_SLASH,
 	EC_DIGIT, EC_DIGIT, EC_DIGIT, EC_DIGIT, EC_DIGIT, EC_DIGIT, EC_DIGIT, EC_DIGIT,
 	EC_DIGIT, EC_DIGIT, EC_SC_OP, EC_SC_OP, EC_LESSER, EC_EQUALS, EC_GREATER, EC_SC_OP,
 	0, EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA,
 	EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA,
 	EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA,
-	EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_SC_OP, 0, EC_SC_OP, EC_EQUABLE, EC_ALPHA,
+	EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_SC_OP, EC_SPACE, EC_SC_OP, EC_EQUABLE, EC_ALPHA,
 	0, EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA,
 	EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA,
 	EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_ALPHA,
 	EC_ALPHA, EC_ALPHA, EC_ALPHA, EC_SC_OP, EC_OR, EC_SC_OP, EC_EQUABLE, 0,
 };
 
-
 /* transition table from [state][equivalence class] -> new state */
-const u8 transitions[10][20] = {
+const u8 transitions[26][20] = {
 	/* initial */
-	{ S_END_OF_FILE, S_SPACE, S_SPACE, S_ID, S_INT, S_ERROR, S_SC_OP, S_EQUABLE, S_EQ, -1,
-	  -1, -1, -1, -1, -1, -1, -1, -1, S_CHAR, S_STRING},
+	{ S_END_OF_FILE, S_SPACE, S_SPACE, S_ID, S_INT, S_DOT, S_SC_OP, S_EQUABLE, S_EQ, S_PLUS,
+	  S_MINUS, S_STAR, S_SLASH, S_AND, S_OR, S_GREATER, S_LESSER, S_CHAR, S_STRING},
 	/* space */
-	{ S_END_OF_FILE, S_SPACE, S_SPACE, S_ID, S_INT, S_ERROR, S_SC_OP,  S_EQUABLE, S_EQ, -1,
-	  -1, -1, -1, -1, -1, -1, -1, -1, S_CHAR, S_STRING},
+	{ S_END_OF_FILE, S_SPACE, S_SPACE, S_ID, S_INT, S_DOT, S_SC_OP,  S_EQUABLE, S_EQ, S_PLUS,
+	  S_MINUS, S_STAR, S_SLASH, S_AND, S_OR, S_GREATER, S_LESSER, S_CHAR, S_STRING},
 	/* id */
-	{ S_FINAL_ID, S_FINAL_ID, S_FINAL_ID, S_ID, S_ID, S_ERROR, S_FINAL_ID, S_FINAL_ID, S_FINAL_ID, S_FINAL_ID,
-	  S_FINAL_ID, S_FINAL_ID, S_FINAL_ID, S_FINAL_ID, S_FINAL_ID, S_FINAL_ID, S_FINAL_ID, S_FINAL_ID, S_FINAL_ID, S_FINAL_ID},
+	{ S_FINAL_ID, S_FINAL_ID, S_FINAL_ID, S_ID, S_ID, S_FINAL_ID, S_FINAL_ID, S_FINAL_ID, S_FINAL_ID, S_FINAL_ID,
+	  S_FINAL_ID, S_FINAL_ID, S_FINAL_ID, S_FINAL_ID, S_FINAL_ID, S_FINAL_ID, S_FINAL_ID, S_FINAL_ID, S_FINAL_ID},
 	/* int */
 	{ S_FINAL_INT, S_FINAL_INT, S_FINAL_INT, S_FINAL_INT, S_INT, S_FLOAT, S_FINAL_INT, S_FINAL_INT, S_FINAL_INT, S_FINAL_INT,
-	  S_FINAL_INT, S_FINAL_INT, S_FINAL_INT, S_FINAL_INT, S_FINAL_INT, S_FINAL_INT, S_FINAL_INT, S_FINAL_INT, S_FINAL_INT, S_FINAL_INT},
+	  S_FINAL_INT, S_FINAL_INT, S_FINAL_INT, S_FINAL_INT, S_FINAL_INT, S_FINAL_INT, S_FINAL_INT, S_FINAL_INT, S_FINAL_INT},
 	/* float */
 	{ S_FINAL_FLOAT, S_FINAL_FLOAT, S_FINAL_FLOAT, S_FINAL_FLOAT, S_FLOAT,
 	  S_ERROR, S_FINAL_FLOAT, S_FINAL_FLOAT, S_FINAL_FLOAT, S_FINAL_FLOAT,
 	  S_FINAL_FLOAT, S_FINAL_FLOAT, S_FINAL_FLOAT, S_FINAL_FLOAT, S_FINAL_FLOAT,
-	  S_FINAL_FLOAT, S_FINAL_FLOAT, S_FINAL_FLOAT, S_FINAL_FLOAT, S_FINAL_FLOAT},
+	  S_FINAL_FLOAT, S_FINAL_FLOAT, S_FINAL_FLOAT, S_FINAL_FLOAT},
 	/* single char operator */
 	{ S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC,
-	  S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC},
+	  S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC},
 	  /* char */
 	{ S_ERROR, S_CHAR, S_ERROR, S_CHAR, S_CHAR, S_CHAR, S_CHAR, S_CHAR, S_CHAR, S_CHAR,
-	  S_CHAR, S_CHAR, S_CHAR, S_CHAR, S_CHAR, S_CHAR, S_CHAR, S_CHAR, S_FINAL_CHAR, S_CHAR},
+	  S_CHAR, S_CHAR, S_CHAR, S_CHAR, S_CHAR, S_CHAR, S_CHAR, S_FINAL_CHAR, S_CHAR},
 	/* string */
-	{ S_ERROR, S_STRING, S_ERROR, S_STRING, S_STRING, S_STRING, S_STRING, S_STRING, S_STRING, S_STRING,
-	  S_STRING, S_STRING, S_STRING, S_STRING, S_STRING, S_STRING, S_STRING, S_STRING, S_STRING, S_FINAL_STRING},
+	{ S_FINAL_STRING, S_STRING, S_STRING, S_STRING, S_STRING, S_STRING, S_STRING, S_STRING, S_STRING, S_STRING,
+	  S_STRING, S_STRING, S_STRING, S_STRING, S_STRING, S_STRING, S_STRING, S_STRING, S_FINAL_STRING},
 	/* equals */
 	{ S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_TC, S_FINAL_SC,
-	  S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC},
+	  S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC},
 	/* equable */
 	{ S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_TC, S_FINAL_SC,
-	  S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC},
+	  S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC},
+	  /* plus */
+	{ S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_TC, S_FINAL_TC,
+	  S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC},
+	  /* minus */
+	{ S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_TC, S_FINAL_SC,
+	  S_FINAL_TC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_TC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC},
+	  /* star: */
+	{ S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_TC, S_FINAL_SC,
+	  S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC},
+	  /* slash */
+	{ S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_TC, S_FINAL_SC,
+	  S_FINAL_SC, S_ML_COMMENT, S_SL_COMMENT, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC},
+	  /* and */
+	{ S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_TC, S_FINAL_SC,
+	  S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_TC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC},
+	  /* or */
+	{ S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_TC, S_FINAL_SC,
+	  S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_TC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC},
+	  /* greater */
+	{ S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_TC, S_FINAL_SC,
+	  S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_RSH, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC},
+	  /* lesser */
+	{ S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_TC, S_FINAL_SC,
+	  S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_LSH, S_FINAL_SC, S_FINAL_SC},
+	  /* single line comment */
+	{ S_END_OF_FILE, S_SL_COMMENT, S_SL_COMMENT_END, S_SL_COMMENT, S_SL_COMMENT, S_SL_COMMENT, S_SL_COMMENT, S_SL_COMMENT, S_SL_COMMENT, S_SL_COMMENT,
+	  S_SL_COMMENT, S_SL_COMMENT, S_SL_COMMENT, S_SL_COMMENT, S_SL_COMMENT, S_SL_COMMENT, S_SL_COMMENT, S_SL_COMMENT, S_SL_COMMENT},
+	  /* single line comment end */
+	{ S_END_OF_FILE, S_SPACE, S_SPACE, S_SPACE, S_SPACE, S_SPACE, S_SPACE, S_SPACE, S_SPACE, S_SPACE,
+	  S_SPACE, S_SPACE, S_SPACE, S_SPACE, S_SPACE, S_SPACE, S_SPACE, S_SPACE, S_SPACE},
+	  /* multi line comment */
+	{ S_END_OF_FILE, S_ML_COMMENT, S_ML_COMMENT, S_ML_COMMENT, S_ML_COMMENT, S_ML_COMMENT, S_ML_COMMENT, S_ML_COMMENT, S_ML_COMMENT, S_ML_COMMENT,
+	  S_ML_COMMENT, S_ML_COMMENT_END, S_ML_COMMENT, S_ML_COMMENT, S_ML_COMMENT, S_ML_COMMENT, S_ML_COMMENT, S_ML_COMMENT, S_ML_COMMENT},
+	  /* multi line comment possible end */
+	{ S_END_OF_FILE, S_ML_COMMENT, S_ML_COMMENT, S_ML_COMMENT, S_ML_COMMENT, S_ML_COMMENT, S_ML_COMMENT, S_ML_COMMENT, S_ML_COMMENT, S_ML_COMMENT,
+	  S_ML_COMMENT, S_ML_COMMENT, S_SPACE, S_ML_COMMENT, S_ML_COMMENT, S_ML_COMMENT, S_ML_COMMENT, S_ML_COMMENT, S_ML_COMMENT},
+	/* dot */
+	{ S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_DOT_DOT, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC,
+	  S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC, S_FINAL_SC},
+	  /* dot dot */
+	{ S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_DOT_DOT_DOT, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC,
+	  S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC},
+	  /* left shift */
+	{ S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_LSH_EQUALS, S_FINAL_TC,
+	  S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC},
+	  /* right shift */
+	{ S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_RSH_EQUALS, S_FINAL_TC,
+	  S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC, S_FINAL_TC},
 };
 
-const u8 in_token[] = {
+
+const s8 in_token[] = {
 	0, 0, 1, 1,
 	1, 1, 1, 1,
+	1, 1, 1, 1,
+	1, 1, 1, 1,
+	1, 1, 0, -1,
+	0, -1, 1, 1,
 	1, 1, 0, 0,
 	0, 0, 0, 0,
-	0, 0, 0,
+	0, 0, 0, 1,
+	1, 1
 };
 
 const u8 state_to_token_type[] = {
@@ -218,6 +291,9 @@ const u8 state_to_token_type[] = {
 	T_CHAR,
 	T_SINGLE_CHAR_OPERATOR,
 	T_TWO_CHAR_OPERATOR,
+	T_DOT_DOT_DOT,
+	T_LSH_EQ,
+	T_RSH_EQ
 };
 
 const char *keywords[] = {
@@ -325,6 +401,11 @@ Token lex(LexerContext *ctx) {
 			tk.lexeme = lexeme;
 		}
 	} break;
+	case T_DOT_DOT_DOT:
+	case T_LSH_EQ:
+	case T_RSH_EQ:
+		p++;
+		break;
 	default:
 		break;
 	}
@@ -367,46 +448,70 @@ const char *token_type_to_str(u8 token_type) {
 	case T_LESS_EQ: return "less equals";
 	case T_LSH_EQ: return "left shift equals";
 	case T_RSH_EQ: return "right shift equals";
+	case T_DOT_DOT_DOT: return "dot dot dot";
 	default: "";
 	}
 
 	return "";
 }
 
+void print_token(Token token) {
+	switch (token.type) {
+	case T_INT_LITERAL:
+		printf("%s: %lld\n", token_type_to_str(token.type), token.int_value);
+		break;
+	case T_FLOAT_LITERAL:
+		printf("%s: %f\n", token_type_to_str(token.type), token.float_value);
+		break;
+	case T_IDENTIFIER:
+	case T_STRING:
+		printf("%s: '%.*s'\n", token_type_to_str(token.type), (u32)token.lexeme.length, token.lexeme.ptr);
+		break;
+	case T_KEYWORD:
+		printf("%s: %d\n", token_type_to_str(token.type), token.keyword);
+		break;
+	case T_CHAR:
+		printf("%s: '%c'\n", token_type_to_str(token.type), token.int_value);
+		break;
+	default:
+		if ((token.type >= T_EQ_EQ && token.type <= T_DOT_DOT_DOT) || token.type == T_ERROR) {
+			printf("%s\n", token_type_to_str(token.type));
+		} else {
+			printf("%c\n", token.type);
+		}
+		break;
+	}
+}
+
+char *read_entire_file(const char *file_name) {
+	FILE *f = fopen(file_name, "rb");
+
+	fseek(f, 0, SEEK_END);
+	size_t len = ftell(f);
+	fseek(f, 0, SEEK_SET);
+
+	char *text = malloc(len + 1);
+	fread(text, 1, len, f);
+	text[len] = 0;
+
+	fclose(f);
+
+	return text;
+}
+
 s32 main() {
-	const char *input = "int main() {\n\tprintf(\"%d\", 512);\n\treturn 0;\n}";
-	
+	char *input = read_entire_file("E:\\Development\\c\\crow\\crow\\lex.c");
+
 	LexerContext ctx;
 	ctx.current = input;
 
+	size_t num_tokens = 0;
+
 	Token token;
 	while ((token = lex(&ctx)).type != T_END_OF_FILE) {
-		switch (token.type) {
-		case T_INT_LITERAL:
-			printf("%s: %lld\n", token_type_to_str(token.type), token.int_value);
-			break;
-		case T_FLOAT_LITERAL:
-			printf("%s: %f\n", token_type_to_str(token.type), token.float_value);
-			break;
-		case T_IDENTIFIER:
-		case T_STRING:
-			printf("%s: '%.*s'\n", token_type_to_str(token.type), (u32) token.lexeme.length, token.lexeme.ptr);
-			break;
-		case T_KEYWORD:
-			printf("%s: %d\n", token_type_to_str(token.type), token.keyword);
-			break;
-		case T_CHAR:
-			printf("%s: '%c'\n", token_type_to_str(token.type), token.int_value);
-			break;
-		default:
-			if ((token.type >= T_EQ_EQ && token.type <= T_RSH_EQ) || token.type == T_ERROR ) {
-				printf("%s\n", token_type_to_str(token.type));
-			} else {
-				printf("%c\n", token.type);
-			}
-			break;
-		}
+		print_token(token);
 	}
+
 
 	return 0;
 }
